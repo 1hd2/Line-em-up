11fcdcc2eeb4c70cf7b48e09dcb172d206380f09
1hd2 <a.y.l.chia@gmail.com>
2026-06-21 18:07:46 +1000

    Getting started - create a player

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
+def main():
+    print("Start of main...")
 
+    in_game = True
+    while in_game:
+        player = generate_player()
 
-print("Starting...")
+    print("End of main...")
+
+main()
---
+++ player.py
@@ -0,0 +1,9 @@
+class player:
+    def __init__(self):
+        self.fire = 5
+        self.water = 5
+        self.metal = 5
+        self.earth = 5
+        self.wood = 5
+        self.draw = 5
+        self.is_alive
